f27f82e7ebbdd0e1e5a37738061caf8140dfc9e0
Julian Applebaum <julianapplebaum@gmail.com>
2013-03-17 20:16:44 -0400

    More docs. Boost the minimum relay cell count to 4. Add writebucketized.py

--- logparse.py
+++ logparse.py
@@ -1,8 +1,31 @@
+"""
+Parse a hack_tor log file, and output the resulting records in JSON.
+Syntax: python logparse.py infile outfile direction
+Direction is either "O" for outgoing cells, or "I" for incoming cells.
+The logfile this outputs is in the format:
+[ { 'ident': [circuit id, ip slug]
+	'create': timestamp of last create cell received
+	'relays': list of timestamps for all relay cells in the circuit
+	'destroy': timestamp of last destroy cell received
+  },
+  { ... },
+  { ... },
+  ...
+]
+"""
+
 from datetime import datetime
 import sys, json
 
-# takes a time string and returns the number of milliseconds since January 1 2013
 def parse_time(time_str):
+	"""
+	Parse a time string from the Tor logfile. Returns the number of milliseconds
+	since January 1, 2013. We use this date instead of the epoch to avoid enormous
+	timestamps.
+
+	@param time_str: The time string from the logfile
+	@return: The number of milliseconds since January 1, 2013
+	"""
     augmented = '2013 ' + time_str
     date_parsed = datetime.strptime(augmented[0:-4],'%Y %b %d %H:%M:%S')
     n_seconds = 1.0*(date_parsed - datetime(2013, 1, 1)).total_seconds()
@@ -11,6 +34,15 @@ def parse_time(time_str):
     return 1000*n_seconds + n_milliseconds
 
 def parse_line(line):
+	"""
+	Parse the circuit id, ip slug, and timestamp from a line in
+	the hack_tor log file.
+	@param line: The line
+	@return: A dictionary {
+		'ident': (circid, ipslug),
+		'time': the timestamp
+	}
+	"""
 	split = line.split(" ")
 	circid = int(split[5])
 	ipslug = int(split[6])
@@ -20,72 +52,63 @@ def parse_line(line):
 		'time': time
 	}
 
-lfpath = sys.argv[1]
-outpath = sys.argv[2]
-direc = sys.argv[3].upper()
-
-with open(lfpath) as logfile:
-	print "Reading file..."
-	create_times = {} # map circuit idents to start times
-	time_series = {} # map circuit idents to time series
-	zero_circ_ids = []
-	n_incomplete = 0
-
-	print "Parsing..."
-	# first pass - determine which circuits are complete
-	for line in logfile:
-		# print "%i circuits so far" % (len(time_series) + 1)
-		if line[29:35] == "CREATE":
-			record = parse_line(line)
-			ident = record['ident']
-			create_time = record['time']
-			create_times[ident] = create_time
-		elif line[29:36] == "DESTROY":
-			record = parse_line(line)
-			ident = record['ident']
-			create_time = create_times.get(ident)
-			if create_time is not None:
-				destroy_time = record['time']
-				# This can actually happen, believe it or not. Likely explanation
-				# is that the CREATE cell suffered more latency than the DESTROY,
-				# so they both arrived at the same time.
-				time_series[ident] = {
-					'create': create_time,
-					'relays': [],
-					'destroy': destroy_time,
-					'ident': ident
-				}
-
-	logfile.seek(0)
-
-	# second pass - build time series for complete circuits
-	print "Adding relay timing data..."
-	for line in logfile:
-		if line[29:33] == "RRC" + direc:
-			record = parse_line(line)
-			series = time_series.get(record['ident'])
-			if series is not None:
-				series['relays'].append(record['time'])
-
-	n_incomplete = len(create_times) - len(time_series)
-
-	# third pass - delete circuits with no relay cells
-	print "Removing circuits with no relay cells"
-	for ident, record in time_series.iteritems():
-		if len(record['relays']) < 2:
-			zero_circ_ids.append(ident)
-
-	for ident in zero_circ_ids:
-		del time_series[ident]
-
-	percent_complete = 100 * 1.0*len(time_series)/len(create_times)
-
-	print "** There were %i circuits total" % len(create_times)
-	print "** %i circuits only had CREATE cells" % n_incomplete
-	print "** %i circuits had CREATE and DESTROY, but no RELAY" % len(zero_circ_ids)
-	print "** %d%% (%i) circuits had CREATE, RELAY, and DESTROY" % (percent_complete,
-		len(time_series))
-
-	with open(outpath, 'w') as outfile:
-		print "Dumping data to %s" % outpath
-		json.dump(time_series.values(), outfile)
+if __name__ == "__main__":
+	lfpath = sys.argv[1]
+	outpath = sys.argv[2]
+	direc = sys.argv[3].upper()
+	with open(lfpath) as logfile:
+		print "Reading file..."
+		create_times = {} # map circuit idents to start times
+		time_series = {} # map circuit idents to time series
+		zero_circ_ids = []
+		n_incomplete = 0
+		print "Parsing..."
+		# First pass - determine which circuits are complete
+		for line in logfile:
+			if line[29:35] == "CREATE":
+				record = parse_line(line)
+				ident = record['ident']
+				create_time = record['time']
+				create_times[ident] = create_time
+			elif line[29:36] == "DESTROY":
+				record = parse_line(line)
+				ident = record['ident']
+				create_time = create_times.get(ident)
+				if create_time is not None:
+					destroy_time = record['time']
+					# This can actually happen, believe it or not. Likely explanation
+					# is that the CREATE cell suffered more latency than the DESTROY,
+					# so they both arrived at the same time.
+					time_series[ident] = {
+						'ident': ident
+						'create': create_time,
+						'relays': [],
+						'destroy': destroy_time,
+					}
+		logfile.seek(0)
+		# Second pass - build time series for complete circuits
+		print "Adding relay timing data..."
+		for line in logfile:
+			if line[29:33] == "RRC" + direc:
+				record = parse_line(line)
+				series = time_series.get(record['ident'])
+				if series is not None:
+					series['relays'].append(record['time'])
+		n_incomplete = len(create_times) - len(time_series)
+		# Third pass - remove invalid circuits. It takes at least 4 relay cells
+		# to build a valid circuit, so we ignore anything less.
+		print "Removing circuits with too few relay cells"
+		for ident, record in time_series.iteritems():
+			if len(record['relays']) < 4:
+				zero_circ_ids.append(ident)
+		for ident in zero_circ_ids:
+			del time_series[ident]
+		percent_complete = 100 * 1.0*len(time_series)/len(create_times)
+		print "** There were %i circuits total" % len(create_times)
+		print "** %i circuits only had CREATE cells" % n_incomplete
+		print "** %i circuits had CREATE and DESTROY, but no RELAY" % len(zero_circ_ids)
+		print "** %d%% (%i) valid, complete circuits" % (percent_complete,
+			len(time_series))
+		with open(outpath, 'w') as outfile:
+			print "Dumping data to %s" % outpath
+			json.dump(time_series.values(), outfile)
---
+++ writebucketized.py
@@ -0,0 +1,65 @@
+"""
+Window the sequences in JSON a file output by logparse.py
+Syntax: python writebucketized infile window_size outfile
+window_size is in milliseconds.
+@author Julian Applebaum
+"""
+
+from pprint import pprint
+import sys, json
+
+def windowize(record, window_size):
+	"""
+	Window a circuit's relay cell time series.
+	@param record: The record encoding the time series
+	@param: window_size: The length, in milliseconds, of each window
+	@return: The windowed version of the time series component.
+	"""
+	start = record['create']
+	end = record['destroy']
+	relays = record['relays']
+	circ_len = end - start
+	# make all times relative to the last CREATE cell sent
+	adj_times = [t - start for t in relays]
+	first_relay = adj_times[0]
+	last_relay = adj_times[-1]
+	if first_relay < 0:
+		raise ValueError("!!! Circuit %s had RELAY before CREATE" % record['ident'])
+	elif last_relay > circ_len:
+		raise ValueError("!!! Circuit %s had RELAY after DESTROY" % record['ident'])
+	n_windows = max(1, int(round(circ_len/window_size + .5)))
+	windowed = []
+	# fill all windows with 0 to start
+	for i in xrange(0, n_windows):
+		windowed.append(0)
+	window_idx = 0
+	window_end = window_size
+	# fill windows with relay cell counts
+	for time in adj_times:
+		if time > window_end:
+			window_idx += 1
+			window_end += window_size
+		windowed[window_idx] += 1
+	return windowed
+
+if __name__ == "__main__":
+	filepath = sys.argv[1]
+	window_size = int(sys.argv[2])
+	outpath = sys.argv[3]
+	with open(filepath) as data_file:
+		with open(outpath, 'w') as out_file:
+			print "Loading circuit data..."
+			circuits = json.load(data_file)
+			print "Windowing %i circuits" % len(circuits)
+			good_circs = []
+			for i in xrange(0, len(circuits)):
+				print "Circuit %i" % i
+				circ = circuits[i]
+				try:
+					windowized = windowize(circ, window_size)
+					circ['relays'] = windowized
+					good_circs.append(circ)
+				except ValueError, e:
+					print e
+			print "Dumping to %s" % outpath
+			json.dump(good_circs, out_file)
